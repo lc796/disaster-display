dc19edd71ba2eab783bdc4408d4328767a4faa82
Luke Collishaw <lc796@exeter.ac.uk>
2022-10-05 17:32:05 +0100

    Add basic routing and 404 page

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,25 @@
 import '../styles/App.css';
 import Navbar from "./Navbar";
 import Footer from "./Footer";
+import {Route, Routes, BrowserRouter as Router} from "react-router-dom";
+import About from "./about/About";
+import Map from "./map/Map";
+import Stats from "./stats/Stats";
+import PageNotFound from "./page-not-found/PageNotFound";
 
 function App() {
   return (
     <div className="App">
-        <Navbar />
-
-        <Footer />
+        <Router>
+            <Navbar />
+            <Routes>
+                <Route path="/" exact element={ <About/> } />
+                <Route path="/map" element={ <Map/> } />
+                <Route path="/stats" element={ <Stats/> } />
+                <Route path="*" element={ <PageNotFound/> } />
+            </Routes>
+            <Footer />
+        </Router>
     </div>
   );
 }
---
+++ src/components/page-not-found/PageNotFound.js
@@ -0,0 +1,9 @@
+const PageNotFound = () => {
+    return (
+        <div className="not-found">
+            <h1>404 - Page Not Found</h1>
+        </div>
+    )
+}
+
+export default PageNotFound;
